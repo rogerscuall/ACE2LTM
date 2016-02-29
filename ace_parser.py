eb78e79a3d25367c3e9720747b6bdb179a145add
Roger Gomez Olivares <rogerscuall@users.noreply.github.com>
2016-02-29 15:14:30 -0500

    Update ace_parser.py
    Small changes.

--- ace_parser.py
+++ ace_parser.py
@@ -1,10 +1,11 @@
 from ciscoconfparse import CiscoConfParse
-#from __future__ import print_function
+# from __future__ import print_function
 import sys
 
-file = '/Users/rgomez/OneDrive/Programs/ACE2LTM/ace.txt'
 
-class ACE():
+# file = '/Users/rgomez/OneDrive/Programs/ACE2LTM/ace.txt'
+
+class ACE:
     def __init__(self, file):
         """
         :param file:
@@ -24,15 +25,14 @@ class ACE():
         self.probes = {}
         self.virtual_server = {}
 
-
     def set_rservers(self):
         list_rserver = self.parser.find_objects(r'^rserver host')
         for i in list_rserver:
             rserver = i.re_search_children('ip address')
             name_rserver = i.text.split(' ')[-1]
-            if rserver == []:
+            if not rserver:
                 print("The Real Server {} don't have any IP".format(name_rserver))
-            #is a list of only one element always
+            # is a list of only one element always
             rserver = rserver[0].text
             ip_rserver = rserver.split(' ')[-1]
             self.rservers[name_rserver] = ip_rserver
@@ -47,14 +47,12 @@ class ACE():
         probe_dict = {'name': probe_name, 'protocol': probe_protocol, 'port': probe_port}
         return probe_dict
 
-
-
     def set_serverfarm(self):
         list_serverfarm = self.parser.find_objects(r'serverfarm host')
         for i in list_serverfarm:
             serverfarm_name = i.text.split(' ')[-1]
             probe = i.re_search_children('probe')
-            #is a list of only one element always
+            # is a list of only one element always
             probe_name = probe[0].text.split(' ')[-1]
             probe = self.find_probe(probe_name)
             try:
@@ -64,9 +62,7 @@ class ACE():
             list_rservers = i.re_search_children('rserver')
             temp_list = []
             for ii in list_rservers:
-                rserver ={}
-                rserver['name'] = (ii.text.split(' ')[-2])
-                rserver['port'] = (ii.text.split(' ')[-1])
+                rserver = {'name': (ii.text.split(' ')[-2]), 'port': (ii.text.split(' ')[-1])}
                 temp_list.append(rserver)
             self.serverfarm[serverfarm_name] = {}
             self.serverfarm[serverfarm_name]['nodes'] = temp_list
@@ -77,24 +73,23 @@ class ACE():
         for i in list_virtual_server:
             virtual_server_name = i.text.split(' ')[-1]
             virtual_address = i.re_search_children(r'2 match virtual-address')
-            #is a list of only one element always
+            # is a list of only one element always
             virtual_address = virtual_address[0].text
             virtual_address_port = virtual_address.split(' ')[-1]
             virtual_address_protocol = virtual_address.split(' ')[-3]
             virtual_address_ip = virtual_address.split(' ')[-4]
-            parentspec = '^class '+ virtual_server_name + '$'
+            parentspec = '^class ' + virtual_server_name + '$'
             policy = self.parser.find_objects_w_parents(parentspec=parentspec,
-                                                       childspec='loadbalance policy')
-            #is a list of only one element always
+                                                        childspec='loadbalance policy')
+            # is a list of only one element always
             try:
                 policy = policy[0].text
                 policy_name = policy.split(' ')[-1]
-                #print policy_name
                 parentspec = 'policy-map type loadbalance first-match ' + policy_name + '$'
-                serverfarm = policy = self.parser.find_objects_w_parents(parentspec=parentspec,
-                                                       childspec='serverfarm ')
+                serverfarm = self.parser.find_objects_w_parents(parentspec=parentspec,
+                                                                childspec='serverfarm ')
 
-                #is a list of only one element always
+                # is a list of only one element always
                 serverfarm = serverfarm[0].text
                 serverfarm_name = serverfarm.split(' ')[-1]
                 self.virtual_server[virtual_server_name] = {'pool': serverfarm_name, 'ip': virtual_address_ip,
@@ -102,8 +97,6 @@ class ACE():
                                                             'protocol': virtual_address_protocol}
             except IndexError:
                 print 'The policy-map type loadbalance with name {} was not found'.format(policy_name)
-
-
 
     def set_probes(self, probe):
         """
@@ -115,7 +108,6 @@ class ACE():
             raise IndexError(probe['name'], 'Value exist already')
         else:
             self.probes[probe['name']] = {'port': probe['port'], 'protocol': probe['protocol']}
-
 
     def cli_add_probes(self):
         cli = 'create ltm monitor {} {} destination *:{}'
@@ -152,8 +144,8 @@ class ACE():
             for ii in self.serverfarm[i]['nodes']:
                 print(cli.format(i, ii['name'], ii['port']))
 
-
-    def _count_length(self, atribute):
+    @staticmethod
+    def _count_length(atribute):
         return len(atribute)
 
     def cli_to_file(self):
@@ -167,9 +159,7 @@ class ACE():
         self.cli_add_virtual_servers()
         output.close()
 
-
     def update_config(self):
         self.set_rservers()
         self.set_serverfarm()
         self.set_virtual_server()
-
